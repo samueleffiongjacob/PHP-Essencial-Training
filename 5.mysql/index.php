12f4cf68715eedd6579ea7b925e5c67e580fec57
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2024-09-19 20:27:47 +0100

    new ports added

--- 5.mysql/index.php
+++ 5.mysql/index.php
@@ -10,14 +10,14 @@
 
 // connecting to database
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1; dbname=EFFIONG', 'root', ''); 
+    $pdo = new PDO('mysql:host=127.0.0.1;port=3307; dbname=effiong', 'root', 'samdev'); // always specify port  should incase it not running on 3306
 } catch (PDOException $e){
     die('could not connect'. $e->getMessage());
 }
 
 
 // fetching data
-$statement= $pdo->prepare('select * from posts');
+$statement= $pdo->prepare('select * from wallets');
 
 $statement->execute();
 
@@ -26,3 +26,4 @@ $results = $statement->fetchAll(PDO::FETCH_OBJ);
 $queryString = $statement->queryString;
 // var_dump($statement->fetchAll());
 require 'index.view.php';
+
